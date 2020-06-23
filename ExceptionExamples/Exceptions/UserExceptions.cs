755a367c7a7935c37f619ed1c413dd428d815ebd
jrfilms89 <jrfilms89@gmail.com>
2020-06-23 15:41:17 -0400

    Add project files.

---
+++ ExceptionExamples/Exceptions/UserExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionExamples {
+    class UserExceptions : Exception{
+
+        public double NumberInError { get; set; }
+
+        public UserExceptions() : base() { }
+
+        public UserExceptions(string message) : base(message) { }
+
+        public UserExceptions(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ ExceptionExamples/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExceptionExamples {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                Run();
+            } catch (UserExceptions ex) {
+                Console.WriteLine($"{ex.NumberInError}: {ex.Message}");
+            } catch (Exception ex) {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+        static void Run() {
+            
+                Test();
+                var ex = new UserExceptions("This exception was forced!");
+                ex.NumberInError = 69;
+                throw ex;
+            
+        }
+
+        static void Test() {
+            try {
+                var num = 1;
+                var denom = 0;
+                var ans = num / denom;
+            } catch (DivideByZeroException ex) {
+                Console.WriteLine($"Exception occured : {ex.Message}");
+            }
+        }
+    }
+}
